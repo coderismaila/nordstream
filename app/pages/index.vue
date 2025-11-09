f80a60332e1df3bdb4a393801b66d2c2ca1d2736
Ismaila Hassan <code.ismailah28@gmail.com>
2025-11-09 17:58:44 +0100

    Add UMain component

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,11 @@
 <template>
+    <UMain>
         <UPageHero title="Nordstream Resource Management Limited"
             description=" Providing Innovative & Sustainable Energy Solutions." :links="links" headline="Coming Soon" />
-        <UFooter class="absolute bottom-6 text-gray-500 text-sm">
-            © {{ new Date().getFullYear() }} Nordstream Resource Management Ltd. All rights reserved.
-        </UFooter>
+    </UMain>
+    <UFooter class="absolute bottom-6 text-gray-500 text-sm">
+        © {{ new Date().getFullYear() }} Nordstream Resource Management Ltd. All rights reserved.
+    </UFooter>
 </template>
 
 <script setup lang="ts">
